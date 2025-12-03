86de48948448aee73b5ae3a838e67fa14b58efb8
AzerSD <sioudazer8@gmail.com>
2025-12-03 12:07:20 +0100

    Seperation of concerns

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,54 +1,47 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 	"strconv"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		app.notFound(w) // Use the notFound() helper
 		return
 	}
-
-	files := []string {
-		"./ui/html/pages/home.tmpl.html",
-		"./ui/html/base.tmpl.html",
-		"./ui/html/partials/nav.tmpl.html",
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/partials/nav.tmpl",
+		"./ui/html/pages/home.tmpl",
 	}
-
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		app.errorLog.Println(err.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		app.serverError(w, err) // Use the serverError() helper.
+		return
 	}
-	
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		app.errorLog.Println(err.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		app.serverError(w, err) // Use the serverError() helper.
 	}
-	
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		app.notFound(w) // Use the notFound() helper.
 		return
 	}
-
-	app.infoLog.Println("Display a specific snippet with ID ...")
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		app.clientError(w, http.StatusMethodNotAllowed) // Use the clientError() helper.
 		return
 	}
-
-	app.infoLog.Println("Creating a new snippet...")
+	w.Write([]byte("Create a new snippet..."))
 }
